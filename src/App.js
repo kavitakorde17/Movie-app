53b5564ad564eb744b3066dcb3640696d3b23183
kkorde <kavitakorde17@gmail.com>
2023-03-03 16:41:42 +0530

    change app name

---
+++ src/App.js
@@ -0,0 +1,47 @@
+// import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import './App.css';
+import Header from "./componant/Header/Header";
+
+function App() {
+  return (
+    <div >
+<Header/>
+    
+    
+    </div>
+  );
+}
+
+export default App;
+
+// import './App.css';
+// import { BrowserRouter, Route, Routes } from 'react-router-dom';
+// import Layout from './pages/Layout';
+// import Login from './pages/Login';
+// import Signup from './pages/Signup';
+// import Services from './pages/Services';
+// import Home from './pages/Home';
+// import Aboutus from './pages/AboutUs';
+// import CustomizedDialogs from './pages/Signup';
+
+
+// function App() {
+//   return (
+//   <>
+//   <BrowserRouter>
+//     <Routes>
+//        <Route path='/' element={<Layout />}  >  
+//        <Route index element={<Home />} />
+//        <Route path='/Home' element={<Home/>}  />
+//        <Route path='/login' element={<Login />}  />
+//         <Route path='/Signup' element={<CustomizedDialogs ><Login /></CustomizedDialogs>}  />
+//         <Route path='/services' element={<Services />} />
+//         <Route path='/aboutus' element={<Aboutus />} />
+//         {/* <Route path='*' element={<Error />}/> */}
+//     </Routes>
+//     </BrowserRouter>
+//   </>)
+//   }
+
+// export default App;
+
